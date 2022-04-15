d93c6608feae80a1f328a95c7ea9a52590165f0f
bryanj408 <bryan.arndt52@gmail.com>
2022-04-15 07:46:17 -0700

    moved files to proper spot

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//console.log() writes in the console in the inspector tab
+//alert() creates a pop-up
+//document.write() actually writes on the page
+
+alert('Hello world');
+/*
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+*/
+
+let favoriteFood = 'thai';
+document.write(favoriteFood);
