9618fc05b6e6b9fc14bdf24eeda214340d801f5a
bryanj408 <bryan.arndt52@gmail.com>
2022-04-16 16:02:03 -0700

    added pokemonList with arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 //console.log() writes in the console in the inspector tab
 //alert() creates a pop-up
 //document.write() actually writes on the page
+
+let pokemonList = [{name: "Eevee", height: 5, type: ["water", "fire", "bolt"]},
+{name: "Weedle", height: 6, type: ["drill", "fire"]},
+{name: "squirtle", height: 7, type: ["water", "strength"]}];
