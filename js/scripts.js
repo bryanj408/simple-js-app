9dbb07b78c7a52114992151f97a074891856c006
bryanj408 <bryan.arndt52@gmail.com>
2022-04-19 12:44:57 -0700

    added for loop and nested if else
    -deleted "initial scale" in html doc
    -added font styling in css
    -added for loop to pokedex

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
 //console.log() writes in the console in the inspector tab
 //alert() creates a pop-up
 //document.write() actually writes on the page
+//&&(and)  ||(or)
 
-let pokemonList = [{name: "Eevee", height: 5, type: ["water", "fire", "bolt"]},
-{name: "Weedle", height: 6, type: ["drill", "fire"]},
-{name: "squirtle", height: 7, type: ["water", "strength"]}];
+//Added 3 pokemon in pokemonList and added arrays to each type
+let pokemonList = [{name: "Eevee", height: 5, type: ["water", " fire", " bolt"]},
+{name: " Weedle", height: 6, type: ["drill", "fire"]},
+{name: " squirtle", height: 7, type: ["water", "strength"]}];
+
+//i=0 sets the iteration to the beginning
+//pokemonList.length is to let the for loop know how many objects are in the arrays
+//i++ is to iterate through the array
+//put spacing where needed in strings
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")")
+  if (pokemonList[i].height <= 5) {
+    document.write(" \n-This is a small pokemon");
+  } else if (pokemonList[i].height === 6) {
+    document.write(" -This is an average pokemon");
+  } else if (pokemonList[i].height >= 7 ) {
+    document.write(" -Wow, that's big!");
+  }
+}
