6329942ebcb4042e5b916151623daf8d6f5d7f8b
bryanj408 <bryan.arndt52@gmail.com>
2022-04-25 10:03:14 -0700

    for loop gone/ foreach() added

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,15 @@
 
 //Added 3 pokemon in pokemonList and added arrays to each type
 let pokemonList = [{name: "Eevee", height: 5, type: ["water", " fire", " bolt"]},
-{name: " Weedle", height: 6, type: ["drill", "fire"]},
-{name: " squirtle", height: 7, type: ["water", "strength"]}];
+{name: " Weedle", height: 6, type: ["drill", " fire"]},
+{name: " Squirtle", height: 7, type: ["water", " strength"]}];
 
 //i=0 sets the iteration to the beginning
 //pokemonList.length is to let the for loop know how many objects are in the arrays
 //i++ is to iterate through the array
 //put spacing where needed in strings
+
+/* 
 for (let i = 0; i < pokemonList.length; i++) {
   document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
   if (pokemonList[i].height <= 5) {
@@ -21,4 +23,10 @@ for (let i = 0; i < pokemonList.length; i++) {
   } else if (pokemonList[i].height >= 7 ) {
     document.write(" -Wow, that's big!" + "<br>");
   }
+}*/
+
+function pokemonListDetails(pokemon) {
+  document.write(pokemon.name + ' (height: ' + pokemon.height + ') ' + pokemon.type + '<br>');
 }
+
+pokemonList.forEach(pokemonListDetails);
